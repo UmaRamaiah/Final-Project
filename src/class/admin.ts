ac077427dabff51435605823d3c27cb6b1e239f2
UmaRamaiah <umaramaiah13@gmail.com>
2024-02-08 19:28:12 +0530

    first commit

---
+++ src/class/admin.ts
@@ -0,0 +1,11 @@
+export class Admin {
+username: string;
+adminName:string;
+adminpassword: any;
+    constructor(adminId:number, adminName:string, adminPhone:string, username:string, adminpassword:string) {
+    this.username = username;
+    this.adminName = adminName;
+    this.adminpassword = adminpassword;
+          
+    }
+}
